9c44bafefb59ae8b8b227ad78de29439848cd4e6
Zaboev <Zaboevsamar@gmail.com>
2025-10-26 14:59:22 +0300

    FeistelStructure

--- src/Main.kt
+++ src/Main.kt
@@ -121,15 +121,15 @@ interface IRoundFunction {
 
 interface IEncrDecr {
 
-    suspend fun encryptionAlgorithm(enBlock: ByteArray, inputFile: String, outputFile: String)
+    suspend fun encryptionAlgorithm(enBlock: ByteArray): ByteArray
 
-    suspend fun decryptionAlgorithm(deBlock: ByteArray, inputFile: String, outputFile: String)
+    suspend fun decryptionAlgorithm(deBlock: ByteArray): ByteArray
 
     suspend fun setRoundKeys(key: ByteArray)
 
 } //Done
 
-class CypherAlgorithm(
+class contextCypherAlgorithm(
 
     private val encryptionKey: ByteArray,
     private val mode: EncryptionMode,
@@ -139,41 +139,21 @@ class CypherAlgorithm(
     private val indexBase: IndexBase,
     vararg extraParams: Any // Исправить
 
-) : IEncrDecr {
+) {
 
-    override suspend fun encryptionAlgorithm(enBlock: ByteArray, inputFile: String, outputFile: String){
+    when (mode) {
 
-
-
-        when(mode){
-
-            EncryptionMode.ECB -> return
-            EncryptionMode.CBC -> return
-            EncryptionMode.PCBC -> return
-            EncryptionMode.CFB -> return
-            EncryptionMode.OFB -> return
-            EncryptionMode.CTR -> return
-            EncryptionMode.RandomDelta -> return
-
-        }
-
-    }
-
-    override suspend fun decryptionAlgorithm(deBlock: ByteArray, inputFile: String, outputFile: String) {
-
-        val i = 0
-
-    }
-
-    override suspend fun setRoundKeys(key: ByteArray) {
-
-        val roundKeysObject = RoundKeysGenerator(endian, indexBase)
-        val roundKeys = roundKeysObject.rKeysGenerator(key)
+        EncryptionMode.ECB -> return
+        EncryptionMode.CBC -> return
+        EncryptionMode.PCBC -> return
+        EncryptionMode.CFB -> return
+        EncryptionMode.OFB -> return
+        EncryptionMode.CTR -> return
+        EncryptionMode.RandomDelta -> return
 
     }
 
 }
-
 
 class RoundKeysGenerator(private val endian: Endian, private val indexBase: IndexBase): IRoundKeysGenerator {
 
@@ -264,7 +244,7 @@ class RoundKeysGenerator(private val endian: Endian, private val indexBase: Inde
 
 }
 
-class RoundFunction(private val block: ByteArray, private val roundKey: ByteArray, private val endian: Endian, private val indexBase: IndexBase) : IRoundFunction {
+class RoundFunction(private val endian: Endian, private val indexBase: IndexBase) : IRoundFunction {
 
     private val expansionPermutation: IntArray = intArrayOf (
         32, 1, 2, 3, 4, 5,
@@ -394,7 +374,7 @@ class RoundFunction(private val block: ByteArray, private val roundKey: ByteArra
 
     } // From AI
 
-    private suspend fun xor (block48: ByteArray): ByteArray {
+    private suspend fun xor (block48: ByteArray, roundKey: ByteArray): ByteArray {
 
         return ByteArray(6) {i ->
 
@@ -407,7 +387,7 @@ class RoundFunction(private val block: ByteArray, private val roundKey: ByteArra
     override suspend fun encryptionTransformation(block: ByteArray, roundKey: ByteArray): ByteArray {
 
         val block48 = bitSwap(block, expansionPermutation, endian, indexBase)
-        val keyedBlock48 = xor(block48)
+        val keyedBlock48 = xor(block48, roundKey)
         val sBoxedBlock32 = sBoxesPermutation(keyedBlock48)
         return bitSwap(sBoxedBlock32, transposition, endian, indexBase)
 
@@ -415,4 +395,104 @@ class RoundFunction(private val block: ByteArray, private val roundKey: ByteArra
 
 }
 
-class FeistelStructure(private val objectFS: IRoundFunction, private val keyGenerator: IRoundKeysGenerator)
+class FeistelStructure(
+    private val objectRF: IRoundFunction,
+    private val keyGenerator: IRoundKeysGenerator,
+    private val endian: Endian,
+    private val indexBase: IndexBase,
+    private val entryKey: ByteArray
+): IEncrDecr {
+
+    private val initialPermutation = intArrayOf (
+
+        58, 50, 42, 34, 26, 18, 10, 2,
+        60, 52, 44, 36, 28, 20, 12, 4,
+        62, 54, 46, 38, 30, 22, 14, 6,
+        64, 56, 48, 40, 32, 24, 16, 8,
+        57, 49, 41, 33, 25, 17, 9, 1,
+        59, 51, 43, 35, 27, 19, 11, 3,
+        61, 53, 45, 37, 29, 21, 13, 5,
+        63, 55, 47, 39, 31, 23, 15, 7
+
+    )
+
+    private val inverseInitialPermutation = intArrayOf (
+
+        40, 8, 48, 16, 56, 24, 64, 32,
+        39, 7, 47, 15, 55, 23, 63, 31,
+        38, 6, 46, 14, 54, 22, 62, 30,
+        37, 5, 45, 13, 53, 21, 61, 29,
+        36, 4, 44, 12, 51, 20, 60, 28,
+        35, 3, 43, 11, 50, 19, 59, 27,
+        34, 2, 42, 10, 49, 18, 58, 26,
+        33, 1, 41, 9, 48, 17, 57, 25
+
+    )
+
+    private var roundKeys = ArrayList<ByteArray>(16)
+
+    private suspend fun xor (left: ByteArray, keyedRight: ByteArray): ByteArray {
+
+        return ByteArray(4) {i ->
+
+            (left[i].toInt() xor keyedRight[i].toInt()).toByte()
+
+        }
+
+    }
+
+    override suspend fun encryptionAlgorithm(enBlock: ByteArray): ByteArray {
+
+        setRoundKeys(entryKey)
+
+        val initialBlock = bitSwap(enBlock, initialPermutation, endian, indexBase)
+        var cipheredBlock = enBlock
+
+        for (round in 1 .. 16) {
+
+            val leftBytes = cipheredBlock.copyOfRange(0, 4)
+            val rightBytes = cipheredBlock.copyOfRange(4, 8)
+
+            val funFBlock = objectRF.encryptionTransformation(rightBytes, roundKeys[round])
+
+            val xored = xor(leftBytes, funFBlock)
+
+            cipheredBlock = rightBytes + xored
+
+        }
+
+        return bitSwap(cipheredBlock, inverseInitialPermutation, endian, indexBase)
+
+    }
+
+    override suspend fun decryptionAlgorithm(deBlock: ByteArray): ByteArray {
+
+        setRoundKeys(entryKey)
+
+        val initialBlock = bitSwap(deBlock, initialPermutation, endian, indexBase)
+        var cipheredBlock = deBlock
+
+        for (round in 16 downTo 1) {
+
+            val leftBytes = cipheredBlock.copyOfRange(0, 4)
+            val rightBytes = cipheredBlock.copyOfRange(4, 8)
+
+            val funFBlock = objectRF.encryptionTransformation(rightBytes, roundKeys[round])
+
+            val xored = xor(leftBytes, funFBlock)
+
+            cipheredBlock = rightBytes + xored
+
+        }
+
+        return bitSwap(cipheredBlock, inverseInitialPermutation, endian, indexBase)
+
+    }
+
+    override suspend fun setRoundKeys(key: ByteArray) {
+
+        roundKeys = keyGenerator.rKeysGenerator(entryKey)
+
+    }
+
+}
